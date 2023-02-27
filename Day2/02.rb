2022642219c083c519a692b4753ff20d254944c2
seungyeons2 <seungyeonsi913@gmail.com>
2023-02-27 15:15:12 +0900

    Day2

--- Day2/02.rb
+++ Day2/02.rb
@@ -1,13 +1,14 @@
-my_string = gets.chomp
-puts "당신이 입력한 문장은: " + my_string
-
+print ">>>first name 내놔: "
 first_name = gets.chomp
 first_name.capitalize!
 
+print ">>>last name 내놔: "
 last_name = gets.chomp
 last_name.capitalize!
 
+print ">>>어디에 사느냐?: "
 state = gets.chomp
 state.upcase!
 
-puts "당신은 #{state}에 거주하는 #{last_name}#{first_name}이군요!"
+puts ">>>당신은 #{state}에 거주하는 #{last_name}\s#{first_name}이군요!"
+
